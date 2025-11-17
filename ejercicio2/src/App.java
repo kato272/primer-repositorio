ad934a23e8fdb9998b388e688462a45e03827d3d
kato272 <id00916229@usal.es>
2025-11-17 19:06:28 +0100

    Delete ejercicio2 directory

--- ejercicio2/src/App.java
+++
@@ -1,7 +0,0 @@
-import Controlador.Controlador;
-public class App {
-    public static void main(String args[]) {
-        Controlador app = new Controlador();  
-        app.iniciar();                         
-    }
-}
