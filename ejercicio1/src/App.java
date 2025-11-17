bb1a0e41267587c335d7d8ec8e16e0e28e5b4abf
kato272 <id00916229@usal.es>
2025-11-17 19:06:22 +0100

    Delete ejercicio1 directory

--- ejercicio1/src/App.java
+++
@@ -1,9 +0,0 @@
-public class App
- {
-    public static void main(String[] args) {
-        Model model = new Model();
-        View view = new View();
-        Controller controller = new Controller(model, view);
-        controller.run();
-    }
-}
