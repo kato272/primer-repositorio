9885fdd2109c58d96fa8cc6cfe497c10bbec1ec5
kato272 <id00916229@usal.es>
2025-11-17 19:25:29 +0100

    Add files via upload

---
+++ semana1/segundo.java
@@ -0,0 +1,18 @@
+import java.util.Scanner; 
+
+public class segundo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+
+        System.out.print("Ingresa el primer número: ");
+        int num1 = scanner.nextInt(); 
+
+        System.out.print("Ingresa el segundo número: ");
+        int num2 = scanner.nextInt(); 
+
+        int suma = num1 + num2; 
+
+        System.out.println("La suma es: " + suma); 
+        scanner.close(); 
+    }
+}
