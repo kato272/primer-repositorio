0c675311a8f927305ab84c7cfec0132de72d2265
kato272 <id00916229@usal.es>
2025-11-27 23:55:03 +0100

    Add files via upload

---
+++ semana77/ejercicio2/src/App.java
@@ -0,0 +1,7 @@
+import Controlador.Controlador;
+public class App {
+    public static void main(String args[]) {
+        Controlador app = new Controlador();  
+        app.iniciar();                         
+    }
+}
